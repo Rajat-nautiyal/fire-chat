2a4f111a08723e822da82538c7ed9f11e5cabcda
Rajat nautiyal <snautiyal048@gmail.com>
2024-02-01 12:54:54 +0530

    Chat-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from "react";
-import {MainPage} from './NEWS_site/mainNewsPage'
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {auth} from './chat-app/firebase.js';
+import { onAuthStateChanged } from 'firebase/auth';
+import {Home} from './chat-app/components/Home.js';
+import {LogInAuth} from './chat-app/components/SignIn.js';
+import SignUpAuth from './chat-app/components/Signup.js';
+import { DataProvider } from './chat-app/contextFile/dataProvider.js';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return 'Loading please wait..';
+  }
 
   return (
-    <div>
-      <MainPage/>
-    </div>
+    <DataProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={user ? <Home /> : <LogInAuth />} />
+          <Route path="/SignUp" element={<SignUpAuth />} />
+          <Route path="/LogIn" element={user ? <Navigate to="/" /> : <LogInAuth />} />
+        </Routes>
+      </BrowserRouter>
+    </DataProvider>
   );
-}
+};
 
 export default App;
